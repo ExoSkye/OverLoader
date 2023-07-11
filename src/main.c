fc2e2b3509a332c92eb7a42fcfef6d0171194c83
Skye <protobyte@duck.com>
2023-07-11 22:53:07 +0100

    kernel: add non-working test for dtb parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stddef.h>
-#include <stdint.h>
+#include <endian.h>
+#include <dtb.h>
+#include <utils.h>
 
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
+    for (uint64_t x = dtb_ptr32; x < sizeof(fdt_header); x += 4) {
+        *((uint32_t*)x) = swap_endian_32(*((uint32_t*)x));
+    }
 
+    fdt_header header = *((fdt_header *)dtb_ptr32);
+
+    volatile uint32_t version = header.version;
+
+    if (header.magic != 0xd00dfeed) {
+        emergency_halt();
+    }
 }
