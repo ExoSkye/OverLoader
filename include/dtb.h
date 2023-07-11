b8f2f9860b2a0b9df4176a43ff467ff3e77dda10
Skye <protobyte@duck.com>
2023-07-11 22:52:47 +0100

    kernel: add dtb header struct

---
+++ include/dtb.h
@@ -0,0 +1,14 @@
+#include <int_types.h>
+
+typedef struct fdt_header {
+    u32 magic;
+    u32 totalsize;
+    u32 off_dt_struct;
+    u32 off_dt_strings;
+    u32 off_mem_rsvmap;
+    u32 version;
+    u32 last_comp_version;
+    u32 boot_cpuid_phys;
+    u32 size_dt_strings;
+    u32 size_dt_struct;
+} fdt_header;
