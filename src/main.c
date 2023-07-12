fb5c25a92eec3cc3403f470f1fe93a25d0e196d2
Skye <protobyte@duck.com>
2023-07-12 07:23:26 +0100

    kernel: fix(ish) the slightly broken dtb code

--- src/main.c
+++ src/main.c
@@ -3,14 +3,12 @@
 #include <dtb.h>
 #include <utils.h>
 
-void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
-    for (uint64_t x = dtb_ptr32; x < sizeof(fdt_header); x += 4) {
-        *((uint32_t*)x) = swap_endian_32(*((uint32_t*)x));
-    }
-
+void kernel_main(u64 dtb_ptr32, u64 x1, u64 x2, u64 x3) {
     fdt_header header = *((fdt_header *)dtb_ptr32);
 
-    volatile uint32_t version = header.version;
+    header.magic = swap_endian_32(header.magic);
+
+    volatile uint32_t version = swap_endian_32(header.version);
 
     if (header.magic != 0xd00dfeed) {
         emergency_halt();
